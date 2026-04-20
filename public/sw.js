f5af79db79f857029c7c74066bbd69a6cc91917d
Felipe <felipe.sdo5324@gmail.com>
2026-04-20 17:09:59 -0300

    Nome aplicativo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'equiprent-shell-v1';
+const CACHE_NAME = 'taxa-tecnoloc-shell-v1';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', (event) => {
